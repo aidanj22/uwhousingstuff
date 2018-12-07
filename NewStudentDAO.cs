66a847c1a0c7dba5f1629401a2a8684dbdd31fc8
aidanj22 <32644482+aidanj22@users.noreply.github.com>
2018-12-07 16:56:04 -0600

    Add files via upload

---
+++ NewStudentDAO.cs
@@ -0,0 +1,34 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using UWAdventure.Entities.Persistence;
+using Dapper;
+using UWHousing.Entities.DTO;
+using UWHousing.Entities.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UWAHousing.Enum;
+
+namespace UWHousing.Data
+{
+    /// <summary>
+    /// Data access object for students 
+    /// </summary>
+    public class StudentDAO
+    {
+
+        /// <summary>
+        /// Creates a new student
+        /// </summary>
+        public void CreateStudent(NewStudentDTO newstudentDTO)
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["uwhousing"].ConnectionString)) //placeholder
+            {
+                connection.Open();
+                string sql //query
+                    
+                connection.Execute(sql, new { newstudentDTO }o);
+            }
+           
+        }
+  
---
+++ PaymentHistoryDAO.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using UWAdventure.Entities.Persistence;
+using Dapper;
+using UWHousing.Entities.DTO;
+using UWHousing.Entities.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UWHousing.Enum;
+
+namespace UWHousing.Data
+{
+    /// <summary>
+    /// Data access object for payment history 
+    /// </summary>
+    public class PaymentDAO
+    {
+
+        /// <summary>
+        /// returns full order details for a single order
+        /// </summary>
+        public PaymentHistoryViewModel GetRunPaymentHistory(int student_id)
+        {
+            IList<PaymentHistoryViewModel> payments = QueryForGetRunPaymentHistory("payments.student_id=@student_id"); //syntax will likely change
+            return payments.Count > 0 ? payments : null; //if this works it doesn't return everything we want/in the format we want it
+        }
+    }
