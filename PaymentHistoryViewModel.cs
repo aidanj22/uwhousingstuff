a2ba2d5489b609525d2eb5643845dd1744acb3e8
aidanj22 <32644482+aidanj22@users.noreply.github.com>
2018-12-07 16:11:22 -0600

    Update PaymentHistoryViewModel.cs

--- PaymentHistoryViewModel.cs
+++ PaymentHistoryViewModel.cs
@@ -9,11 +9,12 @@ namespace UWHousing.Entities.ViewModels
     /// <summary>
     /// View model for payments
     /// </summary>
-    public class OrderItemViewModel //iList of payments within
+    public class IList<PaymentViewModel> //iList of payments within
     {
+        string sql = "SELECT first_name, last_name FROM student AS A INNER JOIN payments AS B A.student_id = B.student_id;"; //may not need aliases
         public PaymentVieweModel StudentID { get; set; }
         //join in data code to join student table with payment table, dapper multiplemapping
-            public PaymentViewModel PaymentAmount { get; set; }
-            public DateTime RequestDate { get; set; }
+        public PaymentViewModel PaymentAmount { get; set; }
+        public DateTime RequestDate { get; set; } //don't need
     }
 }
