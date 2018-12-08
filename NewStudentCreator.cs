136f754aa9a14b4538e5b2e14aff19547d49af21
aidanj22 <32644482+aidanj22@users.noreply.github.com>
2018-12-07 23:38:20 -0600

    Add files via upload

---
+++ NewStudentCreator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UWHousing.Data;
+using UWHousing.Entities.DTO;
+using UWHousing.Entities.Persistence;
+using UWHousing.Enum;
+using UWHousing.Events;
+
+namespace UWHousing.BLL
+{
+    /// <summary>
+    /// Business object responsible for creating new students in the system
+    /// </summary>
+    public class NewStudentCreator
+    {
+        private readonly StudentDAO _studentDAO;
+
+        public event EventHandler<StudentCreatedEventArgs> StudentCreated; //event delegate for when student is created
+
+        public NewStudentCreator()
+        {
+            _studentDAO = new StudentDAO();
+
+        }
+
+        /// <summary>
+        /// Creates a new student
+        /// </summary>
+        public void CreateStudent(NewStudentDTO newstudentDTO)
+        {
+            // create the studentDTO for persistence and populate its properties
+            StudentDTO studentDTO = new StudentDTO()
+            {
+                student_id = newStudentDTO.student_id,
+                student_first_name = newStudentDTO.first_name,
+                student_last_name = newStudentDTO.last_name,
+                building_name = newStudentDTO.building_name,
+                room_number = newstudentDTO.room_number,
+                meal_plan = newstudentDTO.meal_plan,
+            };
+            //formatting/syntax might be off here
+        /// <summary>
+        /// Trigger method called to raise the <see cref="NewStudentCreated"/> event
+        /// </summary>
+        protected virtual void OnStudentCreated(StudentDTO studentDTO)
+        {
+            //null test, without making a copy while keeping thread-safety
+            StudentCreatedEventArgs args = new StudentCreatedEventArgs() { Student = studentDTO };
+            StudentCreated?.Invoke(this, args);
+        }
+    }
+}
