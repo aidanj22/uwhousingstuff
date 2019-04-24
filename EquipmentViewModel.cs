573727e74c0d3f8c662d71eb5676047b89ead23d
aidanj22 <32644482+aidanj22@users.noreply.github.com>
2019-04-24 13:59:47 -0500

    Add files via upload

---
+++ EquipmentViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Housing.Entities.ViewModels
+{
+    /// <summary>
+    /// View model for equipment
+    /// </summary>
+    public class EquipmentViewModel
+    {
+        public long StudentID { get; set; }
+        public string BuildingName { get; set; }
+        public string Status { get; set; }
+
+       /// public override string ToString()
+       /// {
+          ///  if (FirstName == string.Empty || LastName == string.Empty)
+             ///   return base.ToString();
+            ///else
+               /// return string.Format("{0}, {1}", LastName, FirstName);
+
+        ///}
+    }
+}
---
+++ PackageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Housing.Entities.ViewModels
+{
+    /// <summary>
+    /// View model for a package
+    /// </summary>
+    public class PackageViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BuildingName { get; set; }
+        public string Status { get; set; }
+
+        public override string ToString()
+        {
+            if (FirstName == string.Empty || LastName == string.Empty)
+                return base.ToString();
+            else
+                return string.Format("{0}, {1}", LastName, FirstName);
+
+        }
+    }
+}
