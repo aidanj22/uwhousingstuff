f26337ea8d1ea4d6f1f2c590c54a32a6a9a73003
aidanj22 <32644482+aidanj22@users.noreply.github.com>
2019-04-24 13:49:31 -0500

    Add files via upload

---
+++ NewPackageCreator.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Housing.Data;
+using Housing.Entities.DTO;
+using Housing.Entities.Persistence;
+namespace Housing.BLL
+{
+    /// <summary>
+    /// Business object responsible for creating new packages in the system
+    /// </summary>
+    public class NewPackageCreator
+    {
+        private readonly PackageDAO _packageDAO;
+
+
+        public NewPackageCreator()
+        {
+            _PackageDAO = new PackageDAO();
+
+        }
+
+        /// <summary>
+        /// Creates a new package
+        /// </summary>
+        public void CreatePackage(NewPackageDTO newPackageDTO)
+        {
+            // create the studentDTO for persistence and populate its properties
+            PackageDTO ptudentDTO = new PackageDTO()
+            {
+                TrackingNum = newPackageDTO.TrackingNum,
+                Firstname = newPackageDTO.Firstname,
+                Lastname = newPackageDTO.Lastname,
+            };
+
+            _packageDAO.CreatePackage(packageDTO);
+
+        }
+    }
+}
---
+++ PackageDTO.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace Housing.Entities.Persistence
+{
+    /// <summary>
+    /// DTO used for packages being persisted
+    /// </summary>
+    public class PackageDTO
+    {
+
+        public long TrackingNum { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+
+    }
+}
